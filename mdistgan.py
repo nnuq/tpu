367afe984c445d394f87bcbb7e89cfe9063663e4
Roman Ermin <rmermin@yahoo.com>
2020-05-26 15:22:36 +0300

    first commit

--- mdistgan.py
+++ mdistgan.py
@@ -278,7 +278,7 @@ def main(hparams):
     # ------------------------
     # 2 INIT TRAINER
     # ------------------------
-    trainer = Trainer(gpus=1, callbacks=[MyPrintingCallback()], check_val_every_n_epoch=1)
+    trainer = Trainer(num_tpu_cores=8, callbacks=[MyPrintingCallback()], check_val_every_n_epoch=1)
     
     # ------------------------
     # 3 START TRAINING
